6991bc64205b3f98f06ffccfa9211e7ff63e0d52
cpearce31 <cpearce31@gmail.com>
2018-12-27 17:16:39 -0500

    Add folder structure and dummy files
    We'll replace these with the actual components/markup.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.scss'
 import { Route, Link } from 'react-router-dom'
 
-const Dashboard = () => (
-  <div>
-    <h3>Dashboard</h3>
-    <p>This is separate route.</p>
-  </div>
-)
+import About from './App/About'
+import RecentPicks from './App/RecentPicks'
+import Archive from './App/Archive'
+import Publications from './App/Publications'
+import Contact from './App/Contact'
 
 const App = () => (
   <div>
     <nav>
-      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/about">About</Link>
+      <Link to="/recent_picks">Recent Picks</Link>
+      <Link to="/archive">Archive</Link>
+      <Link to="/publications">Publications</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
-    <h1>Welcome to React!</h1>
     <div>
-      <Route path="/dashboard" component={Dashboard}/>
+      <Route
+        path="/about"
+        component={About}
+      />
+      <Route
+        path="/recent_picks"
+        component={RecentPicks}
+      />
+      <Route
+        path="/archive"
+        component={Archive}
+      />
+      <Route
+        path="/publications"
+        component={Publications}
+      />
+      <Route
+        path="/contact"
+        component={Contact}
+      />
     </div>
   </div>
 )
