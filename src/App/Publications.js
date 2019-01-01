671d1d435a341ec77d46ea105b2273cf0d6b9dc5
cpearce31 <cpearce31@gmail.com>
2019-01-01 14:47:04 -0500

    Add prototype of publications page
    Fetches and displays articles; no styling.

--- src/App/Publications.js
+++ src/App/Publications.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react'
+import wretch from 'wretch'
+
+import apiUrl from '../apiConfig'
 
 class Publications extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      publications: null
+    }
+  }
+
   componentDidMount () {
-    console.log('do ajax here')
+    return wretch(`${apiUrl}/articles`)
+      .get().json()
+      .then(res => this.setState({ publications: res.articles }))
+      .catch(err => console.error(err))
   }
 
   render () {
-    return (
+    const { publications } = this.state
+    return publications && (
       <div>
         <h2>
           Publications
         </h2>
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
-        </p>
+        {(publications.map(pub => (
+          <article key={pub.id}>
+            <img
+              src={pub.photo}
+              alt={pub.title}
+            />
+            <h4>
+              {pub.title}
+            </h4>
+            <h5>
+              {pub.subtitle}
+            </h5>
+            <p>
+              {pub.summary}
+            </p>
+          </article>
+        )))}
       </div>
     )
   }
