4bb8ff8312ded904f8a62cdb5147d8555f64e037
cpearce31 <cpearce31@gmail.com>
2019-01-04 15:06:32 -0500

    Revert "Hopefully fix deployment"
    This reverts commit 6434cd97c5c4e0e441b0aeca3c2531276b5ae1d1.

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'http://mighty-river-89051.herokuapp.com',
+  production: 'https://mighty-river-89051.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
